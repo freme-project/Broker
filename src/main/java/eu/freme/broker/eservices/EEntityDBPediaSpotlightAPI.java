c774685772748cbf3a3c7a4cb35825825ba83553
jnehring <jan.nehring@dfki.de>
2015-05-06 16:46:02 +0200

    freme-project/e-Services#2 move e-entity to broker

---
+++ src/main/java/eu/freme/broker/eservices/EEntityDBPediaSpotlightAPI.java
@@ -0,0 +1,58 @@
+package eu.freme.broker.eservices;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+import eu.freme.conversion.rdf.RDFConstants;
+import eu.freme.conversion.rdf.RDFConversionService;
+import eu.freme.eservices.eentity.api.EEntityAPI;
+import eu.freme.eservices.eentity.exceptions.ExternalServiceFailedException;
+
+@RestController
+public class EEntityDBPediaSpotlightAPI extends BaseRestController {
+
+	@Autowired
+	EEntityAPI entityAPI;
+
+	@Autowired
+	RDFConversionService rdfConversionService;
+
+	@RequestMapping(value = "/e-entity/dbpedia-spotlight", method = {
+			RequestMethod.POST, RequestMethod.GET })
+	public ResponseEntity<String> execute(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestHeader(value = "Accept", defaultValue = "") String acceptHeader) {
+
+		String rdf;
+		try {
+			rdf = entityAPI.callDBPediaSpotlight(text);
+		} catch (ExternalServiceFailedException e) {
+			e.printStackTrace();
+			return externalServiceFailedResponse();
+		}
+
+		// get output format
+		RDFConstants.RDFSerialization outputFormat = getOutputSerialization(acceptHeader);
+		System.out.println(rdf);
+		try {
+			Model model = rdfConversionService.unserializeRDF(rdf, RDFConstants.RDFSerialization.TURTLE);
+			String serialization = rdfConversionService.serializeRDF(model,
+					outputFormat);
+			return new ResponseEntity<String>(serialization, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
