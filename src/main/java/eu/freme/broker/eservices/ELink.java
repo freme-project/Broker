966a66030fc8d5270571046b4489f101b796c10a
Milan Dojchinovski <dojcinovski.milan@gmail.com>
2015-05-11 17:13:10 +0200

    e-Link and e-Entity API improvement

--- src/main/java/eu/freme/broker/eservices/ELink.java
+++ src/main/java/eu/freme/broker/eservices/ELink.java
@@ -15,6 +15,8 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 import eu.freme.conversion.rdf.RDFConstants;
 import eu.freme.eservices.elink.DataEnricher;
 import eu.freme.eservices.elink.exceptions.TemplateNotFoundException;
+import java.io.ByteArrayInputStream;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class ELink extends BaseRestController {
@@ -24,45 +26,78 @@ public class ELink extends BaseRestController {
 
 	@RequestMapping(value = "/e-link/", method = RequestMethod.POST)
 	public ResponseEntity<String> enrich(
-			@RequestParam(value = "inputType") String inputType,
 			@RequestParam(value = "templateid", defaultValue = "") int templateId,
-			@RequestParam(value = "input") String input,
-			@RequestHeader(value = "Accept") String acceptHeader) {
+			@RequestHeader(value = "Accept") String acceptHeader,
+			@RequestHeader(value = "Content-Type") String contentTypeHeader,
+                        @RequestParam(value = "informat", defaultValue = "") String informat,
+                        @RequestParam(value = "outformat", defaultValue = "") String outformat,
+                        @RequestBody String body) {
+            try {
+                if( body == null || body.trim().length() == 0 ){
+                    return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
+                }
 
-		if (!validateInputType(inputType)) {
-			return new ResponseEntity<String>("Invalid input-type",
-					HttpStatus.BAD_REQUEST);
-		}
+                Model model = ModelFactory.createDefaultModel();
+                String serOutFormat = "";
 
-		Model model = ModelFactory.createDefaultModel();
+                switch(contentTypeHeader) {
+                    case "text/turtle":
+                        model.read(new ByteArrayInputStream(body.getBytes()), null, "TTL");
+                        break;
+                    case "application/rdf+xml":
+                        model.read(new ByteArrayInputStream(body.getBytes()), null, "RDF/XML");
+                        break;
+                    case "application/ld+json":
+                        model.read(new ByteArrayInputStream(body.getBytes()), null, "JSON-LD");
+                        break;
+                }
+                
+                if(informat != null || informat.trim().length() != 0) {
+                    switch(informat) {
+                        case "turtle":
+                            model.read(new ByteArrayInputStream(body.getBytes()), null, "TTL");
+                            break;
+                        case "rdfxml":
+                            model.read(new ByteArrayInputStream(body.getBytes()), null, "RDF/XML");
+                            break;
+                        case "json-ld":
+                            model.read(new ByteArrayInputStream(body.getBytes()), null, "JSON-LD");
+                            break;
+                    }
+                }
+                
+                serOutFormat = acceptHeader;
+                
+                if(outformat != null || outformat.trim().length() != 0) {
+                    switch(outformat) {
+                        case "turtle":
+                            serOutFormat = "text/turtle";
+                            break;
+                        case "rdfxml":
+                            serOutFormat = "application/rdf+xml";
+                            break;
+                        case "json-ld":
+                            serOutFormat = "application/ld+json";
+                            break;
+                    }
+                }
 
-		if (!inputType.equals(inputTypePlaintext)) {
-			try {
-				model = unserializeNIF(input, inputType);
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new ResponseEntity<String>("invalid nif",
-						HttpStatus.BAD_REQUEST);
-			}
-		}
-
-		try {
-			dataEnricher.enrichNIF(model, templateId);
-		} catch (TemplateNotFoundException e) {
-			return new ResponseEntity<String>("template not found",
-					HttpStatus.BAD_REQUEST);
-		}
+                model = dataEnricher.enrichNIF(model, templateId);
 
 		// get output format
-		RDFConstants.RDFSerialization outputFormat = getOutputSerialization(acceptHeader);
+		RDFConstants.RDFSerialization outputFormat = getOutputSerialization(serOutFormat);
 		String serialization;
 		try {
-			serialization = rdfConversionService.serializeRDF(model,
-					outputFormat);
-		} catch (Exception e) {
-			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+                    serialization = rdfConversionService.serializeRDF(model, outputFormat);
+		} catch (Exception ex) {
+                    return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-
 		return new ResponseEntity<String>(serialization, HttpStatus.OK);
+                
+            } catch (TemplateNotFoundException ex) {
+                System.out.println("template not found");
+    //            Logger.getLogger(ELinkAPI.class.getName()).log(Level.INFO, null, ex);
+            }
+            return null;
 	}
 }
