396cc132eea14028c88ba66edced2e725886f002
jan <gmnoname@gmx.de>
2015-05-13 12:45:21 +0200

    #7 update e-translation to new tilde api

--- src/test/java/eu/freme/broker/TildeETranslationTest.java
+++ src/test/java/eu/freme/broker/TildeETranslationTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.HttpRequestWithBody;
@@ -29,8 +30,8 @@ public class TildeETranslationTest {
 
 	@Test
 	public void testPlaintextEtranslate() throws UnirestException {
-
-		assertTrue(createBaseRequest().asString().getStatus() == HttpStatus.OK
+		HttpResponse<String> response = createBaseRequest().asString();
+		assertTrue(response.getStatus() == HttpStatus.OK
 				.value());
 
 		// TODO add NIF validator here
@@ -53,20 +54,16 @@ public class TildeETranslationTest {
 		
 		String input = readFile("src/test/resources/rdftest/test.turtle");
 		String inputType = "text/turtle";
-		String clientId = "u-bd13faca-b816-4085-95d5-05373d695ab7";
 		String sourceLang = "en";
 		String targetLang = "de";
-		String translationSystemId = "smt-76cd2e73-05c6-4d51-b02f-4fc9c4d40813";
 
 		Unirest
 				.post(baseUrl
-						+ "e-translate/tilde?input={input}&input-type={inputType}&client-id={clientId}&source-lang={sourceLang}&target-lang={targetLang}&translation-system-id={translationSystemId}")
+						+ "e-translation/tilde?input={input}&input-type={inputType}&source-lang={sourceLang}&target-lang={targetLang}")
 				.routeParam("input", input)
 				.routeParam("inputType", inputType)
-				.routeParam("clientId", clientId)
 				.routeParam("sourceLang", sourceLang)
-				.routeParam("targetLang", targetLang)
-				.routeParam("translationSystemId", translationSystemId);
+				.routeParam("targetLang", targetLang);
 	}
 
 	/**
@@ -77,19 +74,16 @@ public class TildeETranslationTest {
 	private HttpRequestWithBody createBaseRequest() {
 		String input = "hello world";
 		String inputType = "plaintext";
-		String clientId = "u-bd13faca-b816-4085-95d5-05373d695ab7";
 		String sourceLang = "en";
 		String targetLang = "de";
-		String translationSystemId = "smt-76cd2e73-05c6-4d51-b02f-4fc9c4d40813";
 
 		return Unirest
 				.post(baseUrl
-						+ "e-translate/tilde?input={input}&input-type={inputType}&client-id={clientId}&source-lang={sourceLang}&target-lang={targetLang}&translation-system-id={translationSystemId}")
-				.routeParam("input", input).routeParam("inputType", inputType)
-				.routeParam("clientId", clientId)
+						+ "e-translation/tilde?input={input}&input-type={inputType}&source-lang={sourceLang}&target-lang={targetLang}")
+				.routeParam("input", input)
+				.routeParam("inputType", inputType)
 				.routeParam("sourceLang", sourceLang)
-				.routeParam("targetLang", targetLang)
-				.routeParam("translationSystemId", translationSystemId);
+				.routeParam("targetLang", targetLang);
 	}
 
 	@After
