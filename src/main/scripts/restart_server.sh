6b517fe03a2ea91c32eddc7737dd0c6861ca7e3a
jan <gmnoname@gmx.de>
2015-05-19 13:44:10 +0200

    freme-project/technical-discussion#12

--- src/main/scripts/restart_server.sh
+++ src/main/scripts/restart_server.sh
@@ -1,4 +1,6 @@
-BASEDIR=$(dirname $0)
-cd ..
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+cd $BASEDIR"/.."
+
 kill `cat config/pid.txt`
 bin/start_server.sh
--- src/main/scripts/start_local.sh
+++ src/main/scripts/start_local.sh
@@ -1,1 +1,4 @@
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+cd $BASEDIR"/..
 java -cp "./*:conf" org.springframework.boot.loader.JarLauncher
