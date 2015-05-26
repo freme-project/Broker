31dc82a5dd70a80c05ad67a6f2e2a4622b90dfa5
Milan Dojchinovski <dojcinovski.milan@gmail.com>
2015-05-26 17:06:39 +0200

    - code refactoring, and impl. of NIF public API

--- src/main/java/eu/freme/broker/eservices/DBpediaSpotlight.java
+++ src/main/java/eu/freme/broker/eservices/DBpediaSpotlight.java
@@ -18,7 +18,6 @@ import com.hp.hpl.jena.vocabulary.RDF;
 
 import eu.freme.broker.exception.ExternalServiceFailedException;
 import eu.freme.broker.tools.NIFParameterSet;
-import eu.freme.conversion.rdf.RDFConstants;
 import eu.freme.conversion.rdf.RDFConversionService;
 import eu.freme.eservices.eentity.api.EEntityService;
 import eu.freme.eservices.eentity.exceptions.BadRequestException;
@@ -28,7 +27,7 @@ import java.io.ByteArrayInputStream;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-public class DBPediaSpotlight extends BaseRestController {
+public class DBpediaSpotlight extends BaseRestController {
 
 	@Autowired
 	EEntityService entityAPI;
@@ -39,30 +38,58 @@ public class DBPediaSpotlight extends BaseRestController {
 	@RequestMapping(value = "/e-entity/dbpedia-spotlight", method = {
             RequestMethod.POST, RequestMethod.GET })
 	public ResponseEntity<String> execute(
-			@RequestParam(value = "input", defaultValue = "") String input,
-			@RequestParam(value = "informat", defaultValue = "") String informat,
-			@RequestParam(value = "i", defaultValue = "") String i,
-			@RequestParam(value = "outformat", defaultValue = "") String outformat,
-			@RequestParam(value = "o", defaultValue = "") String o,
-			@RequestParam(value = "prefix", defaultValue = "") String prefix,
-			@RequestParam(value = "p", defaultValue = "") String p,
-			@RequestHeader(value = "Accept", defaultValue = "") String acceptHeader,
-			@RequestHeader(value = "Content-Type", defaultValue = "") String contentTypeHeader,
-			@RequestParam(value = "language", defaultValue = "") String languageParam,
-			@RequestParam(value = "confidence", defaultValue = "") String confidenceParam,
+			@RequestParam(value = "input", required = false) String input,
+			@RequestParam(value = "i", required = false) String i,
+                        // text, text/turtle, application/json+ld
+			@RequestParam(value = "informat", required = false) String informat,
+			@RequestParam(value = "f", required = false) String f,
+                        // text, text/turtle, application/json+ld
+			@RequestParam(value = "outformat", required = false) String outformat,
+			@RequestParam(value = "o", required = false) String o,
+			@RequestParam(value = "prefix", required = false) String prefix,
+			@RequestParam(value = "p", required = false) String p,
+			@RequestHeader(value = "Accept", required = false) String acceptHeader,
+			@RequestHeader(value = "Content-Type", required = false) String contentTypeHeader,
+			@RequestParam(value = "language", required = false) String languageParam,
+			@RequestParam(value = "confidence", required = false) String confidenceParam,
                         @RequestBody(required = false) String postBody) {
-            
+
             NIFParameterSet parameters = this.normalizeNif(input, informat, outformat, postBody, acceptHeader, contentTypeHeader, prefix);            
-            
+           
             Model inModel;
             Model outModel = ModelFactory.createDefaultModel();;
-                
+
             String textForProcessing = null;
             
-            if(!input.equals("")) {
-                textForProcessing = input;
-                
-            } else if (postBody != null) {
+            // text, text/turtle, application/json+ld
+            String readFrom = null;
+            if(informat != null) {
+                readFrom = informat;
+            } else if(f != null) {
+                readFrom = f;                
+            } else if (contentTypeHeader != null) {
+                readFrom = contentTypeHeader;
+            } else {
+                throw new BadRequestException("Neither informat/i param nor the Content-Type header were set.");
+            }
+                        
+            if(readFrom.equals("text")) {
+                // reading the output from the input parameter
+                if(input != null) {
+                    textForProcessing = input;
+                // reading the output from the i (short form) parameter
+                } else if (i != null) {
+                    textForProcessing = i;
+                } else {
+                    // input and i param are not set
+                    // return bad request
+                    throw new BadRequestException("You set the informat param to text, however, neither input nor i param was set.");
+                }                
+            } else {
+                // the content is sent as body
+                if(contentTypeHeader == null) {
+                    throw new BadRequestException("Neither informat nor Content-Type header was set.");
+                }
             
                 inModel = ModelFactory.createDefaultModel();
                 
@@ -76,54 +103,37 @@ public class DBPediaSpotlight extends BaseRestController {
                         inModel.read(new ByteArrayInputStream(postBody.getBytes()), null, "TTL");
                         break;
                         
-                    case "application/ld+json":
-                        inModel.read(new ByteArrayInputStream(postBody.getBytes()), "JSON-LD");
-                        break;
-                        
-                    case "application/rdf+xml":
-                        inModel.read(new ByteArrayInputStream(postBody.getBytes()), "RDF/XML");
+                    case "application/json+ld":
+                        inModel.read(new ByteArrayInputStream(postBody.getBytes()), null, "JSON-LD");
                         break;
                 }
-                    
                 
                 StmtIterator iter = inModel.listStatements(null, RDF.type, inModel.getResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#Context"));
                 if(iter.hasNext()) {
                     Resource contextRes = iter.nextStatement().getSubject();
                     Statement isStringStm = contextRes.getProperty(inModel.getProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#isString"));
-                    textForProcessing = isStringStm.getObject().asLiteral().getString();
+                    textForProcessing = isStringStm.getObject().asLiteral().getString();                    
                 }
             }
             try {
                 String dbpediaSpotlightRes = entityAPI.callDBpediaSpotlight(textForProcessing, confidenceParam, languageParam);
                 outModel.read(new ByteArrayInputStream(dbpediaSpotlightRes.getBytes()), null, "TTL");
+            } catch (BadRequestException e) {
+                logger.error("failed", e);
+                throw new eu.freme.broker.exception.BadRequestException(e.getMessage());
+            } catch (eu.freme.eservices.eentity.exceptions.ExternalServiceFailedException e) {
+                logger.error("failed", e);
+                throw new ExternalServiceFailedException();
             }
-            catch (BadRequestException e) {
-				logger.error("failed", e);
-				throw new eu.freme.broker.exception.BadRequestException(e.getMessage());
-			} catch (eu.freme.eservices.eentity.exceptions.ExternalServiceFailedException e) {
-				logger.error("failed", e);
-				throw new ExternalServiceFailedException();
-			}
+            
             String serialization;
             try {
-                serialization = serialization = rdfConversionService.serializeRDF(outModel,
-					parameters.getOutformat());
+                serialization = rdfConversionService.serializeRDF(outModel, parameters.getOutformat());
             } catch (Exception e) {
                 logger.error("failed", e);
                 return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
             
-            return new ResponseEntity<String>(serialization, HttpStatus.OK);            
-            // get output format
-//            RDFConstants.RDFSerialization outputFormat = getOutputSerialization(acceptHeader);
-//            try {
-//		Model model = rdfConversionService.unserializeRDF(outModel,
-//		RDFConstants.RDFSerialization.TURTLE);
-//                String serialization = rdfConversionService.serializeRDF(model, outputFormat);
-//		return new ResponseEntity<String>(serialization, HttpStatus.OK);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-//            }
+            return new ResponseEntity<String>(serialization, HttpStatus.OK);
         }
 }
--- src/main/java/eu/freme/broker/eservices/ELink.java
+++ src/main/java/eu/freme/broker/eservices/ELink.java
@@ -1,103 +1,150 @@
-//package eu.freme.broker.eservices;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.RequestHeader;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.hp.hpl.jena.rdf.model.Model;
-//import com.hp.hpl.jena.rdf.model.ModelFactory;
-//
-//import eu.freme.conversion.rdf.RDFConstants;
-//import eu.freme.eservices.elink.DataEnricher;
-//import eu.freme.eservices.elink.exceptions.TemplateNotFoundException;
-//import java.io.ByteArrayInputStream;
-//import org.springframework.web.bind.annotation.RequestBody;
-//
-//@RestController
-//public class ELink extends BaseRestController {
-//
-//	@Autowired
-//	DataEnricher dataEnricher;
-//
-//	@RequestMapping(value = "/e-link/", method = RequestMethod.POST)
-//	public ResponseEntity<String> enrich(
-//			@RequestParam(value = "templateid", defaultValue = "") int templateId,
-//			@RequestHeader(value = "Accept") String acceptHeader,
-//			@RequestHeader(value = "Content-Type") String contentTypeHeader,
-//                        @RequestParam(value = "informat", defaultValue = "") String informat,
-//                        @RequestParam(value = "outformat", defaultValue = "") String outformat,
-//                        @RequestBody String body) {
-//            try {
-//                if( body == null || body.trim().length() == 0 ){
-//                    return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
-//                }
-//
-//                Model model = ModelFactory.createDefaultModel();
-//                String serOutFormat = "";
-//
-//                switch(contentTypeHeader) {
-//                    case "text/turtle":
-//                        model.read(new ByteArrayInputStream(body.getBytes()), null, "TTL");
-//                        break;
-//                    case "application/rdf+xml":
-//                        model.read(new ByteArrayInputStream(body.getBytes()), null, "RDF/XML");
-//                        break;
-//                    case "application/ld+json":
-//                        model.read(new ByteArrayInputStream(body.getBytes()), null, "JSON-LD");
-//                        break;
-//                }
-//                
-//                if(informat != null || informat.trim().length() != 0) {
-//                    switch(informat) {
-//                        case "turtle":
-//                            model.read(new ByteArrayInputStream(body.getBytes()), null, "TTL");
-//                            break;
-//                        case "rdfxml":
-//                            model.read(new ByteArrayInputStream(body.getBytes()), null, "RDF/XML");
-//                            break;
-//                        case "json-ld":
-//                            model.read(new ByteArrayInputStream(body.getBytes()), null, "JSON-LD");
-//                            break;
-//                    }
-//                }
-//                
-//                serOutFormat = acceptHeader;
-//                
-//                if(outformat != null || outformat.trim().length() != 0) {
-//                    switch(outformat) {
-//                        case "turtle":
-//                            serOutFormat = "text/turtle";
-//                            break;
-//                        case "rdfxml":
-//                            serOutFormat = "application/rdf+xml";
-//                            break;
-//                        case "json-ld":
-//                            serOutFormat = "application/ld+json";
-//                            break;
-//                    }
-//                }
-//
-//                model = dataEnricher.enrichNIF(model, templateId);
-//
-//		// get output format
-//		RDFConstants.RDFSerialization outputFormat = getOutputSerialization(serOutFormat);
-//		String serialization;
-//		try {
-//                    serialization = rdfConversionService.serializeRDF(model, outputFormat);
-//		} catch (Exception ex) {
-//                    return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//		return new ResponseEntity<String>(serialization, HttpStatus.OK);
-//                
-//            } catch (TemplateNotFoundException ex) {
-//                System.out.println("template not found");
-//    //            Logger.getLogger(ELinkAPI.class.getName()).log(Level.INFO, null, ex);
-//            }
-//            return null;
-//	}
-//}
+package eu.freme.broker.eservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import eu.freme.conversion.rdf.RDFConstants.RDFSerialization;
+import eu.freme.eservices.elink.DataEnricher;
+import eu.freme.eservices.elink.exceptions.TemplateNotFoundException;
+import java.io.ByteArrayInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class ELink extends BaseRestController {
+
+	@Autowired
+	DataEnricher dataEnricher;
+
+	@RequestMapping(value = "/e-link/", method = RequestMethod.POST)
+	public ResponseEntity<String> enrich(
+			@RequestParam(value = "templateid", required=true) int templateId,
+                        @RequestParam(value = "informat", required=false) String informat,
+                        @RequestParam(value = "f", required=false) String f,
+                        @RequestParam(value = "outformat", required=false) String outformat,
+                        @RequestParam(value = "o", required=false) String o,
+			@RequestHeader(value = "Accept", required=false) String acceptHeader,
+			@RequestHeader(value = "Content-Type", required=false) String contentTypeHeader,
+                        @RequestBody String postBody) {
+            try {
+
+                if( postBody == null || postBody.trim().length() == 0 ){
+                    return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
+                }
+
+                Model model = ModelFactory.createDefaultModel();
+                String serOutFormat = "";
+                
+                if(informat != null ) {
+                    model = readModel(model, informat, postBody);
+                    if(model == null)
+                        return new ResponseEntity<String>("Invalid informat parameter.", HttpStatus.BAD_REQUEST);
+                } else if (f != null) {
+                    model = readModel(model, f, postBody);
+                    if(model == null)
+                        return new ResponseEntity<String>("Invalid informat parameter.", HttpStatus.BAD_REQUEST);
+                } else if (contentTypeHeader != null) {
+                    model = readModel(model, contentTypeHeader, postBody);
+                    if(model == null)
+                        return new ResponseEntity<String>("Invalid Content-Type value.", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+                } else {
+                    return new ResponseEntity<String>("Input format not specified."
+                            + "Use the parameters informat or f, or using the Content-Type header set the input format.", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+                }
+                
+                if(outformat != null) {
+                    switch(outformat) {
+                        case "turtle":
+                            serOutFormat = "text/turtle";
+                            break;
+                        case "text/turtle":
+                            serOutFormat = "text/turtle";
+                            break;
+                        case "json-ld":
+                            serOutFormat = "application/ld+json";
+                            break;
+                        case "application/json+ld":
+                            serOutFormat = "application/ld+json";
+                            break;
+                        default:
+                            return new ResponseEntity<String>("Specified output format is not supported.", HttpStatus.NOT_ACCEPTABLE);
+                    }
+                } else if (o != null) {
+                    
+                    switch(o) {
+                        case "turtle":
+                            serOutFormat = "text/turtle";
+                            break;
+                        case "text/turtle":
+                            serOutFormat = "text/turtle";
+                            break;
+                        case "json-ld":
+                            serOutFormat = "application/ld+json";
+                            break;
+                        case "application/json+ld":
+                            serOutFormat = "application/ld+json";
+                            break;
+                        default:
+                            return new ResponseEntity<String>("Specified output format is not supported.", HttpStatus.NOT_ACCEPTABLE);
+                    }
+                    
+                } else if (acceptHeader != null) {
+                    switch(acceptHeader) {
+                        case "text/turtle":
+                            serOutFormat = "text/turtle";
+                            break;
+                        case "json-ld":
+                            serOutFormat = "application/ld+json";
+                            break;
+                        default:
+                            return new ResponseEntity<String>("Specified output format is not supported.", HttpStatus.NOT_ACCEPTABLE);
+//                            throw new BadRequestException("Unsupported output format: " + acceptHeader);
+                    }
+                } else {
+                    // the output format is not set with the parameters or with the accept header
+                    // the default is turtle
+                    serOutFormat = "text/turtle";
+                }
+
+                model = dataEnricher.enrichNIF(model, templateId);
+                
+                String serialization;
+                
+                switch(serOutFormat) {
+                    case "text/turtle":
+                        serialization = rdfConversionService.serializeRDF(model, RDFSerialization.TURTLE);
+                        return new ResponseEntity<String>(serialization, HttpStatus.OK);                
+                        
+                    case "application/ld+json":
+                        serialization = rdfConversionService.serializeRDF(model, RDFSerialization.JSON_LD);
+                        return new ResponseEntity<String>(serialization, HttpStatus.OK);
+                }
+            } catch (TemplateNotFoundException ex) {
+                Logger.getLogger(ELink.class.getName()).log(Level.INFO, null, ex);
+            } catch (Exception ex) {
+                Logger.getLogger(ELink.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return null;
+	}
+
+    private Model readModel(Model model, String format, String postBody) {
+        if(format.equals("turtle") || format.equals("text/turtle")) {
+            model.read(new ByteArrayInputStream(postBody.getBytes()), null, "TTL");
+        } else if (format.equals("json-ld") || format.equals("application/json+ld")) {
+            model.read(new ByteArrayInputStream(postBody.getBytes()), null, "JSON-LD");
+        } else {
+            return null;
+        }
+        return model;
+    }
+        
+}
