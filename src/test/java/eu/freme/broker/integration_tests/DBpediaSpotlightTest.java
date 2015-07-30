d6b6d1f943394b5caf4c5059af7f006ed1a1d3ca
ArneBinder <ArneBinder@users.noreply.github.com>
2015-07-30 17:42:00 +0200

    implemented DBpediaSpotlightTest

---
+++ src/test/java/eu/freme/broker/integration_tests/DBpediaSpotlightTest.java
@@ -0,0 +1,100 @@
+package eu.freme.broker.integration_tests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import com.hp.hpl.jena.shared.AssertionFailureException;
+import com.mashape.unirest.request.HttpRequest;
+import eu.freme.conversion.rdf.*;
+import org.junit.Test;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.HttpRequestWithBody;
+import org.nlp2rdf.cli.Validate;
+
+
+/**
+ * Created by jonathan on 28.07.15.
+ */
+public class DBpediaSpotlightTest extends IntegrationTest{
+
+
+    String[] availableLanguages = {"en","de","it","nl","fr","es"};
+    String testinput= "Enrich this Content please";
+
+
+    public DBpediaSpotlightTest(){super("/e-entity/dbpedia-spotlight/");}
+
+    protected HttpRequestWithBody baseRequestPost(String function) {
+        return super.baseRequestPost(function).queryString("confidence", "0.2");
+    }
+
+    protected HttpRequest baseRequestGet(String function) {
+        return super.baseRequestGet(function).queryString("confidence", "0.1");
+    }
+
+
+    @Test
+    public void TestDBpediaSpotlightNER() throws UnirestException, IOException, UnsupportedEncodingException {
+
+        HttpResponse<String> response;
+
+        String testinputEncoded= URLEncoder.encode(testinput, "UTF-8");
+        String data = readFile("src/test/resources/rdftest/e-entity/data.ttl");
+
+        //Tests every language
+        for (String lang : availableLanguages) {
+
+            //Tests POST
+            //Plaintext Input in Query String
+            response = baseRequestPost("documents")
+                    .queryString("input", testinput)
+                    .queryString("language", lang)
+                    .queryString("informat", "text")
+                    .asString();
+            validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
+
+            //Tests POST
+            //Plaintext Input in Body
+            response = baseRequestPost("documents")
+                    .queryString("language", lang)
+                    .header("Content-Type", "text/plain")
+                    .body(testinput)
+                    .asString();
+            validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
+            //Tests POST
+            //NIF Input in Body (Turtle)
+            response = baseRequestPost("documents").header("Content-Type", "text/turtle")
+                    .queryString("language", lang)
+                    .body(data).asString();
+            validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
+
+
+            //Tests POST
+            //Test Prefix
+            response = baseRequestPost("documents")
+                    .queryString("input", testinput)
+                    .queryString("language", lang)
+                    .queryString("informat", "text")
+                    .queryString("prefix", "http://test-prefix.com")
+                    .asString();
+            validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
+
+            //assertTrue(response.getString() contains prefix)
+
+            //Tests GET
+            //response = Unirest.get(url+"documents?informat=text&input="+testinputEncoded+"&language="+lang+"&dataset="+dataset).asString();
+            response = baseRequestGet("documents")
+                    .queryString("informat", "text")
+                    .queryString("input", testinputEncoded)
+                    .queryString("language", lang)
+                    .asString();
+            validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
+        }
+    }
+}
