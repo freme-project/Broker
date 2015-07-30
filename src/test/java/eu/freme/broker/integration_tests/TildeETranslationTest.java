cf77549264c7922c7f2bfe5853944f6f420536bb
Jonathan Paul Sauder <j.sauder@campus.tu-berlin.de>
2015-07-30 11:44:08 +0200

    Improved TildeETranslationTest

--- src/test/java/eu/freme/broker/integration_tests/TildeETranslationTest.java
+++ src/test/java/eu/freme/broker/integration_tests/TildeETranslationTest.java
@@ -13,6 +13,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.HttpRequestWithBody;
 
 import eu.freme.conversion.rdf.*;
+import org.nlp2rdf.cli.Validate;
 
 
 public class TildeETranslationTest extends IntegrationTest{
@@ -35,11 +36,15 @@ public class TildeETranslationTest extends IntegrationTest{
 
 		HttpResponse<String> response = baseRequest()
 				.queryString("input", "hello world")
-				.queryString("informat", "text").asString();
+				.queryString("informat", "text")
+				.queryString("outformat","rdf-xml")
+				.asString();
 		assertTrue(response.getStatus() == 200);
 
 		assertTrue(response.getBody().length() > 0);
-		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.TURTLE));
+		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.RDF_XML));
+		// validate NIF
+		Validate.main(new String[]{"-i", response.getBody(), "--informat", "rdfxml"});
 
 		String data = readFile("src/test/resources/rdftest/e-translate/data.turtle");
 		response = baseRequest().header("Content-Type", "text/turtle")
@@ -48,20 +53,44 @@ public class TildeETranslationTest extends IntegrationTest{
 		assertTrue(response.getStatus() == 200);
 		assertTrue(response.getBody().length() > 0);
 		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.TURTLE));
+		// validate NIF
+		Validate.main(new String[]{"-i", response.getBody(),"--informat","turtle"});
 
 		data = readFile("src/test/resources/rdftest/e-translate/data.json");
 		response = baseRequest().header("Content-Type", "application/json+ld")
+				.queryString("outformat","json-ld")
 				.body(data).asString();
 		assertTrue(response.getStatus() == 200);
 		assertTrue(response.getBody().length() > 0);
-		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.TURTLE));
+		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.JSON_LD));
+		// validate NIF
+		//// Validator: informat=json-ld not implemented yet
+		//Validate.main(new String[]{"-i", response.getBody(), "--informat", "json-ld"});
 		
 		data = readFile("src/test/resources/rdftest/e-translate/data.txt");
 		response = baseRequest()
 				.queryString("input", URLEncoder.encode(data, "UTF-8"))
-				.queryString("informat", "text").asString();
+				.queryString("informat", "text")
+				.queryString("outformat","n3")
+				.asString();
 		assertTrue(response.getStatus() == 200);
 		assertTrue(response.getBody().length() > 0);
-		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.TURTLE));
+		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.N3));
+		// validate NIF
+		//// Validator: informat=n3 not implemented yet
+		//Validate.main(new String[]{"-i", response.getBody(), "--informat","n3"});
+
+
+		response = baseRequest()
+				.queryString("input", URLEncoder.encode(data, "UTF-8"))
+				.queryString("informat", "text")
+				.queryString("outformat","n-triples")
+				.asString();
+		assertTrue(response.getStatus() == 200);
+		assertTrue(response.getBody().length() > 0);
+		assertNotNull(converter.unserializeRDF(response.getBody(), RDFConstants.RDFSerialization.N_TRIPLES));
+		// validate NIF
+		//// Validator: informat=ntriples not implemented yet
+		//Validate.main(new String[]{"-i", response.getBody(), "--informat","ntriples"});
 	}
 }
