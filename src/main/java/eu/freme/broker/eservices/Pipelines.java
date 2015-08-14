6facdba98c8dfbb99ceccb20c1676beb52c5e0ae
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2015-08-14 15:41:50 +0200

    support e-Publishing 0.3

--- src/main/java/eu/freme/broker/eservices/Pipelines.java
+++ src/main/java/eu/freme/broker/eservices/Pipelines.java
@@ -1,7 +1,8 @@
 package eu.freme.broker.eservices;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
+import eu.freme.broker.exception.InternalServerErrorException;
 import eu.freme.eservices.pipelines.core.PipelineService;
+import eu.freme.eservices.pipelines.core.ServiceException;
 import eu.freme.eservices.pipelines.requests.RequestBuilder;
 import eu.freme.eservices.pipelines.requests.RequestFactory;
 import eu.freme.eservices.pipelines.requests.SerializedRequest;
@@ -10,12 +11,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import eu.freme.eservices.pipelines.core.PipelineResponse;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -23,7 +21,7 @@ import java.util.List;
  */
 @RestController
 @SuppressWarnings("unused")
-public class Pipelines {
+public class Pipelines extends BaseRestController {
 
 	@Autowired
 	PipelineService pipelineAPI;
@@ -36,14 +34,27 @@ public class Pipelines {
 	 * <p>Examples can be found in the unit tests in the Pipelines repository.</p>
 	 * @param requests	The requests to send to the service.
 	 * @return          The response of the last request.
-	 * @throws IOException
-	 * @throws UnirestException
+	 * @throws InternalServerErrorException		Something goes wrong that shouldn't go wrong.
 	 */
-	@RequestMapping(value = "/pipelining/chain", method = RequestMethod.POST)
-	public ResponseEntity<String> pipeline(@RequestBody String requests) throws IOException, UnirestException {
-		MultiValueMap<String, String> headers = new HttpHeaders();
-		headers.add(HttpHeaders.CONTENT_TYPE, "text/plain");
+	@RequestMapping(value = "/pipelining/chain",
+			method = RequestMethod.POST,
+			consumes = "application/json",
+			produces = {"text/turtle", "application/json", "application/ld+json", "application/n-triples", "application/rdf+xml", "text/n3"}
+	)
+	public ResponseEntity<String> pipeline(@RequestBody String requests) {
 		List<SerializedRequest> serializedRequests = RequestFactory.fromJson(requests);
-		return new ResponseEntity<>(pipelineAPI.chain(serializedRequests), headers, HttpStatus.OK);
+		try {
+			PipelineResponse pipelineResult = pipelineAPI.chain(serializedRequests);
+			MultiValueMap<String, String> headers = new HttpHeaders();
+			headers.add(HttpHeaders.CONTENT_TYPE, pipelineResult.getContentType());
+			return new ResponseEntity<>(pipelineResult.getBody(), headers, HttpStatus.OK);
+		} catch (ServiceException serviceError) {
+			MultiValueMap<String, String> headers = new HttpHeaders();
+			headers.add(HttpHeaders.CONTENT_TYPE, serviceError.getResponse().getContentType());
+			return new ResponseEntity<>(serviceError.getMessage(), headers, serviceError.getStatus());
+		} catch (Throwable t) {
+			// throw a FREME exception if anything goes really wrong...
+			throw new InternalServerErrorException(t.getMessage());
+		}
 	}
 }
