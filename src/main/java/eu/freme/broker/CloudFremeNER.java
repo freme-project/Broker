c1ff95554b10ca2a9e560f898626db1bdb61679e
jnehring <gmnoname@gmx.de>
2015-10-02 15:34:25 +0200

    #106 register FREME NER with eureka

---
+++ src/main/java/eu/freme/broker/CloudFremeNER.java
@@ -0,0 +1,35 @@
+package eu.freme.broker;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.ComponentScan.Filter;
+
+import eu.freme.broker.tools.StarterHelper;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "eu.freme.broker", excludeFilters = @Filter(type = FilterType.REGEX, pattern = { "eu.freme.broker.security.*" }))
+@Profile("cloud-freme-ner")
+@EnableDiscoveryClient
+@Import(FremeNERStarter.class)
+/**
+ * Start FREME ner as part of FREME cloud
+ * 
+ * @author Jan Nehring- jan.nehring@dfki.de
+ *
+ */
+public class CloudFremeNER {
+	static Logger logger = Logger.getLogger(Broker.class);
+
+	public static void main(String[] args){
+		logger.info("Starting FREME NER in cloud mode");
+		String[] newArgs = StarterHelper.addProfile(args, "fremener,cloud-freme-ner");
+		SpringApplication.run(CloudFremeNER.class, newArgs);
+	}
+}
+
